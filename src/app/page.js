5bf76e6210e68c4f8d3614f2c354395dc676fe23
syshenglw <135681592+syshenglw@users.noreply.github.com>
2023-11-18 22:31:10 +0000

    Update frontend to output generated at 6adcdc0

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,4 @@
-import Search from '../app/components/Search';
-import Login from '../app/components/Login';
-import Post from '../app/components/Post';
-import Profile from '../app/components/Profile';
-
+import LandingPage from "@/components/landingpage"
 export default function Home() {
   return (
     <div>
