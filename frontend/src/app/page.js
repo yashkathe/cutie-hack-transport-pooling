4bb5d86088c84d463c6cc1a0b5fc0f7626082501
Stanley Lew <stanley43595@gmail.com>
2023-11-18 14:50:44 -0800

    rearranged folders

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,8 +1,8 @@
-import LandingPage from "@/app/components/landingpage"
-import Search from '../app/components/Search';
-import Login from '../app/components/Login';
-import Post from '../app/components/Post';
-import Profile from '../app/components/Profile';
+import LandingPage from "@/components/landingpage"
+import Search from '../components/Search';
+import Login from '../components/Login';
+import Post from '../components/Post';
+import Profile from '../components/Profile';
 export default function Home() {
   return (
     <div>
