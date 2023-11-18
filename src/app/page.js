2df5d7a31b0b92962d640d85e1370a7f868636e6
syshenglw <135681592+syshenglw@users.noreply.github.com>
2023-11-18 22:38:55 +0000

    Update frontend to output generated at bb51ed7

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
-import LandingPage from "@/components/landingpage"
+import LandingPage from "@/app/components/landingpage"
 import Search from '../app/components/Search';
 import Login from '../app/components/Login';
 import Post from '../app/components/Post';
 import Profile from '../app/components/Profile';
-
+import LandingPage from "@/app/components/landingpage";
 export default function Home() {
   return (
     <div>
-      <div>Commute Connect</div>
+      <LandingPage/>
     </div>
   )
 }
