dba03e359ebf9d5e242d30bf7b46105588ccbcba
Stanley Lew <stanley43595@gmail.com>
2023-11-18 15:03:44 -0800

    added files for pages

---
+++ frontend/src/app/search/page.js
@@ -0,0 +1,8 @@
+import Search from '../components/Search';
+export default function Search() {
+  return (
+    <div>
+        <Search/>
+    </div>
+  )
+}
--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,5 +1,3 @@
-
-
 const Post = () => {
   return (
     <div>
