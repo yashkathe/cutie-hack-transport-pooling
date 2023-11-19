0959f1315ab9010836a0b2715c5ef95537ac00d8
syshenglw <135681592+syshenglw@users.noreply.github.com>
2023-11-19 02:17:33 +0000

    Update frontend to output generated at cb69d05

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,18 @@
 import Link from 'next/link';
+import img1 from "../../public/images/landingpagebg.jpg"
 
 const Login = () => {
   return (
     <div>
+      <div>
+        <img className = "absolute opacity-40" src = {img1.src}/>
+      </div>
+      <p className = "relative text-rose-800 tracking-wider font-serif pt-28 text-center font-black text-7xl">CommuteConnect</p>
+      <p className = "relative text-emerald-700 tracking-wider font-serif pt-12 text-center font-black text-4xl">Login</p>
+      <div className = "absolute top-0 left-10"> 
+        <Link href = "/" className = " bg-white opacity-80 text-rose-800 whitespace-nowrap text-lg p-3 mt-4 rounded-md">Home</Link>
+      </div>
       <div className="rounded border p-4">
-        <div>
-          <div className='text-red-500 font-bold text-xl'>CommuteConnect</div>
-          <div className='text-green-500 font-bold text-xl'>LOGIN</div>
-        </div>
         <div className="mt-4">
           <div className="flex items-center mb-2">
             <div className="inline mr-2">Username:</div>
@@ -17,9 +22,6 @@ const Login = () => {
             <div className="inline mr-2">Password:</div>
             <input type="password" placeholder="password" className="shadow rounded w-3/4 p-2" />
           </div>
-          <Link className='text-blue-500' href="/">
-            Back to Main Page
-          </Link>
         </div>
       </div>
     </div>
--- src/components/landingpage.jsx
+++ src/components/landingpage.jsx
@@ -7,22 +7,22 @@ const LandingPage = () => {
                 <img className = "absolute opacity-40" src = {img1.src}/>
             </div>
 
-            <p className = "relative text-rose-800 tracking-wider font-serif pt-28 text-center font-black text-7xl">CommuteConnect</p>
+            <p className = "relative text-indigo-700 tracking-wider font-serif pt-28 text-center font-black text-7xl">CommuteConnect</p>
 
             <div className = "absolute top-0 right-24"> 
-                <Link href = "/signup" className = " bg-white opacity-80 text-rose-800 whitespace-nowrap text-lg p-3 mt-4 rounded-md">Sign Up</Link>
+                <Link href = "/signup" className = " bg-gray-900 opacity-80 text-indigo-400 whitespace-nowrap text-lg p-3 mt-4 rounded-md">Sign Up</Link>
             </div>
 
             <div className = "absolute top-0 right-4 z-20"> 
-                <Link href = "/login" className = " bg-white opacity-80 text-rose-800 whitespace-nowrap text-lg p-3 mt-4 rounded-md">Login</Link>
+                <Link href = "/login" className = " bg-gray-900 opacity-80 text-indigo-400 whitespace-nowrap text-lg p-3 mt-4 rounded-md">Login</Link>
             </div>
            
-            <div className = "grid grid-cols-2 gap-16 absolute bottom-4 w-full p-4 place-items-center mx-16 max-w-screen-xl">
-                
-                <div className = "bg-orange-100 border border-gray-500 px-28 rounded-lg font-serif">
-                    <p className = " text-rose-800 text-2xl font-bold text-center whitespace-nowrap mt-4">Why Use CommuteConnect</p>
+            <div className = "grid grid-cols-2 gap-12 bottom-0 w-full p-4 place-items-center mt-24 bg-stone-950">
+
+                <div className = "bg-gray-900 border border-gray-500 px-28 rounded-lg font-serif mt-8">
+                    <p className = " text-indigo-400 text-2xl font-bold text-center whitespace-nowrap mt-4">Why Use CommuteConnect</p>
                     <br></br>
-                    <p className = "text-lg">According to the World Health Organization, air pollution kills an estimated seven million people worldwide annually, 
+                    <p className = "text-lg text-white">According to the World Health Organization, air pollution kills an estimated seven million people worldwide annually, 
                         making air pollution the <a className = "font-black">4th </a>leading cause of death worldwide. <br></br><br></br> With vehicle emissions as one of the 
                         leading causes of air pollution, we decided to create 
                         <a className = "font-black"> CommuteConnect</a>. CommuteConnect allows you to coordinate carpooling to your destinations, allowing for a decrease in 
@@ -30,10 +30,10 @@ const LandingPage = () => {
                     </p>
                 </div>
 
-                <div className = "bg-orange-100 border border-gray-500 px-28 pb-8 rounded-lg font-serif">
-                    <p className = " text-rose-800 text-2xl font-bold whitespace-nowrap mt-4">How To Use CommuteConnect</p>
+                <div className = "bg-gray-900 border border-gray-500 px-28 pb-6 rounded-lg font-serif mt-7">
+                    <p className = " text-indigo-400 text-2xl font-bold whitespace-nowrap mt-4">How To Use CommuteConnect</p>
                     <br></br>
-                    <ol className = "text-lg" style={{ listStyleType: "decimal"}}>
+                    <ol className = "text-lg text-white" style={{ listStyleType: "decimal"}}>
                         <li>
                             If you already have an account, click <a className = "font-black">Login</a> at the top right. If you don't already have an account, click
                             <a className = "font-black"> Sign Up</a>.
@@ -46,8 +46,9 @@ const LandingPage = () => {
                         </li>
                         <li>Click <a className = "font-black">Chat</a> to contact the user that best matches your travel plans. Happy Traveling!</li>
                     </ol>
+                    
                 </div>
-
+                <br></br>
             </div>
         </div>
     );
