38832d887e9452604bbab7cee626cb2d61db3ee4
yashkathe <yashkathe123@gmail.com>
2023-11-19 03:54:49 +0000

    Update backend to output generated at 2604ae2

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const Post = require("../models/post-model");
 const User = require("../models/user-model");
 
+const regex = require('../services/regex')
+
 exports.createPost = async (req, res, next) => {
 	const {
 		title,
@@ -84,11 +86,41 @@ exports.getAllPosts = async (req, res, next) => {
 	let posts;
 
 	try {
+		console.log(posts);
 		posts = await Post.find();
 		return res.status(200).json({ message: "OK", posts });
 	} catch (error) {
 		return res.status(500).json({ message: "ERROR", cause: error.message });
 	}
+};
+
+exports.getPostsByQuery = async (req, res, next) => {
+
+    let { query } = req.query;
+    console.log(query)
+	query = regex(query);
+
+	const regexPattern = new RegExp(query, "i");
+
+	let post;
+	try {
+		post = await Post.find({
+			$or: [
+				{ title: { $regex: regexPattern } },
+				{ description: { $regex: regexPattern } },
+				{ destinationPinCode: { $regex: regexPattern } },
+				{ userLocation: { $regex: regexPattern } },
+			],
+		});
+	} catch (err) {
+		console.log(err);
+	}
+
+	res
+		.status(200)
+		.json({
+			post: post.map((post) => post.toObject({ getters: true })),
+		});
 };
 
 exports.updatePostByID = async (req, res, next) => {
